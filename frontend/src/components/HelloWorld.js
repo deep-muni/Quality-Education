0aeeeec3f6985a8c18c209be120a1db65fbe4502
Deep Muni <deep.muni94@gmail.com>
2020-10-26 22:22:25 -0300

    Boilerplate code

---
+++ frontend/src/components/HelloWorld.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/HelloWorld.css';
+
+const HelloWorld = () => {
+    return (
+        <div className="helloworld">
+            Hello World
+        </div>
+    );
+};
+
+export default HelloWorld;
