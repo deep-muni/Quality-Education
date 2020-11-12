5769cc39efded7287b0a9b4f40c7e0eddd80a0d7
tarpara <tarpara.dhruv@dal.ca>
2020-11-11 21:04:16 -0400

    implemented createprofile api

--- backend/route/RegisterRoute.js
+++ backend/route/RegisterRoute.js
@@ -4,5 +4,6 @@ const registerController = require("../controller/RegisterController");
 
 registerRoute.get("/getprofile", registerController.getProfile);
 registerRoute.put("/updateprofile", registerController.updateProfile);
+registerRoute.post("/createProfile", registerController.createProfile);
 
 module.exports = registerRoute;
