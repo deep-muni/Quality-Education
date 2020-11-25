49d6d08720c2f6790d0b49bef590a7b67a3e5338
Dhruv Dalwadi <dh844999@dal.ca>
2020-11-25 19:58:35 -0400

    image fix

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import logo from "../img/logo.png";
+import deepmuni from "../img/deepmuni.jpg";
+import simrandiwan from "../img/simrandiwan.jpg";
+import dhruvtarpara from "../img/dhruvtarpara.jpg";
+import dhruvdalwadi from "../img/dhruvdalwadi.jpg";
 import Header from "./Header";
 class About extends Component {
     state = {  }
@@ -77,7 +81,7 @@ class About extends Component {
                 <div className="row" style={{margin:"5vh"}}>
                     <div className="col-sm-8 col-md-6 col-lg-3 mx-auto mb-3">
                         <div className="card" style={{width:"100%"}}>
-                            <img src="https://scontent.fyaw1-1.fna.fbcdn.net/v/t1.0-9/69331413_2530402660316090_1814845070944239616_o.jpg?_nc_cat=109&ccb=2&_nc_sid=09cbfe&_nc_ohc=WGGn-y9rT6sAX8cMr47&_nc_ht=scontent.fyaw1-1.fna&oh=cb4a350d6a03b378e670389e612fdff9&oe=5FDD7378" className="card-img-top" alt="..."/>
+                            <img src={deepmuni} className="card-img-top" alt="..."/>
                                 <div className="card-body">
                                     <h5 className="card-title">Deep Muni</h5>
                                     <p className="card-text">Expert in developing web applications using Node.js, React.js, and Angular.js. Majorily a front-end developer.</p>
@@ -87,7 +91,7 @@ class About extends Component {
                     </div>
                     <div className="col-sm-8 col-md-6 col-lg-3 mx-auto mb-3">
                         <div className="card" style={{width:"100%"}}>
-                            <img src="https://media-exp1.licdn.com/dms/image/C5603AQEI25K2cllUMQ/profile-displayphoto-shrink_800_800/0?e=1611792000&v=beta&t=MwcmXAeNcJPryEpRI5hxGcfDj8FuvqlC4CMKa2JVDlw" className="card-img-top" alt="..."/>
+                            <img src={dhruvtarpara} className="card-img-top" alt="..."/>
                                 <div className="card-body">
                                     <h5 className="card-title">Dhruv Tarpara</h5>
                                     <p className="card-text">Major area of interest in working on back-end using Java. Moreover, he also worked on different cloud platforms.</p>
@@ -97,7 +101,7 @@ class About extends Component {
                     </div>
                     <div className="col-sm-8 col-md-6 col-lg-3 mx-auto mb-3">
                         <div className="card" style={{width:"100%"}}>
-                            <img src="https://scontent.fyaw1-1.fna.fbcdn.net/v/t1.0-9/121967907_2139821692817174_7210929821271490191_o.jpg?_nc_cat=109&ccb=2&_nc_sid=09cbfe&_nc_ohc=zELle-fBGKAAX_kECXv&_nc_ht=scontent.fyaw1-1.fna&oh=d44355d741dc257f86f476514cc43dec&oe=5FDEF76A" className="card-img-top" alt="..."/>
+                            <img src={simrandiwan} className="card-img-top" alt="..."/>
                                 <div className="card-body">
                                     <h5 className="card-title">Simranbanu Diwan</h5>
                                     <p className="card-text">Developed many web applications using Node.js and React.js. Majorily a back-end developer.</p>
@@ -107,7 +111,7 @@ class About extends Component {
                     </div>
                     <div className="col-sm-8 col-md-6 col-lg-3 mx-auto mb-3">
                         <div className="card" style={{width:"100%"}}>
-                            <img src="https://scontent.fyaw1-1.fna.fbcdn.net/v/t1.0-9/116101625_359611021869424_7588676929496362341_o.jpg?_nc_cat=102&ccb=2&_nc_sid=09cbfe&_nc_ohc=4xgEUL0GwnwAX_jI3gR&_nc_ht=scontent.fyaw1-1.fna&oh=ecffee6cc6f45c40f4dc27f3126b8260&oe=5FE07BAE" className="card-img-top" alt="..."/>
+                            <img src={dhruvdalwadi} className="card-img-top" alt="..."/>
                                 <div className="card-body">
                                     <h5 className="card-title">Dhruv Dalwadi</h5>
                                     <p className="card-text">Strong knowledge base in MERN (MongoDB, Express.js, React.js, and Node.js) technologies.</p>
